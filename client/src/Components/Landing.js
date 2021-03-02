7df22a695f806d4cbf4e5330540662ed66823e4b
Jake Peterson <a3sir86@gmail.com>
2021-03-02 13:25:09 -0700

    Fixes siteinfo positioning and adds attribution for Pexels photo

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -4,21 +4,18 @@ function Landing(props) {
 
     return (
         <div className="section d-flex h-100" id="homesection">
-            <div className="container">
-                <div className="row">
-                    <div className="section-content" id={props.id}>
-                        <div className="container p-2 rounded quotecontainer">
-                            <h2 className="p-2" id="quote">I am a Developer.</h2>
-                        </div>
-                    </div>
-                </div>
-                <div className="row justify-content-center">
-                    <div id="siteBox">
-                        <p className="pl-1 pr-2" id="siteInfo">This site is built with the MERN stack, uses Gitlab for CI/CD, and is deployed in a GKE cluster</p>
-                    </div>
+
+            <div className="section-content" id={props.id}>
+                <div className="container p-2 rounded quotecontainer">
+                    <h2 className="p-2" id="quote">I am a Developer.</h2>
                 </div>
             </div>
+            <div className=" siteInfo">
+            <p className="pl-1 pr-2 mb-0">This site is built with the MERN stack, uses Gitlab for CI/CD, and is deployed in a GKE cluster</p>
+            <p className="pl-1 pr-2 pb-0 mb-0">Background image photo by Eberhard Grossgasteiger from <a href="https://www.pexels.com/photo/mountain-under-starry-sky-1624504/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></p>
+            </div>
         </div>
+
     );
 
 }
