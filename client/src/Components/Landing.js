73cbf71a23aed5ab2fbd287684130b2160605c23
Jake Peterson <a3sir86@gmail.com>
2021-04-10 18:36:46 +0000

    Update Landing.js

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -11,7 +11,7 @@ function Landing(props) {
                 </div>
             </div>
             <div className="siteInfo">
-            <p className="pl-1 pr-2 mb-0">This site is built with the MERN stack, uses Gitlab for CI/CD, and is deployed in a GKE cluster</p>
+            <p className="pl-1 pr-2 mb-0">This site is built with the MERN stack, uses Gitlab for CI/CD, and is deployed in a Microk8s cluster</p>
             <p className="pl-1 pr-2 pb-0 mb-0">Background image photo by Eberhard Grossgasteiger from <a href="https://www.pexels.com/photo/mountain-under-starry-sky-1624504/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></p>
             </div>
         </div>
