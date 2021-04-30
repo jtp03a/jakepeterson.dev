b6cbac6aeff5f7a9390b4f2f7ea5b4d77c931e07
Jake Peterson <a3sir86@gmail.com>
2021-04-30 21:31:06 +0000

    Update client/src/Components/Portfolio.js, client/src/App.scss files

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -3,26 +3,26 @@ import Carousel from 'react-bootstrap/Carousel';
 import homelab from "../Images/homelab.jpg";
 
 function Portfolio(props) {
-    // Get the modal
-    var modal = document.getElementById("myModal");
+    // // Get the modal
+    // var modal = document.getElementById("myModal");
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById("myImg");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onClick = function () {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
+    // // Get the image and insert it inside the modal - use its "alt" text as a caption
+    // var img = document.getElementById("myImg");
+    // var modalImg = document.getElementById("img01");
+    // var captionText = document.getElementById("caption");
+    // img.onClick = function () {
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    // }
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function () {
+    //     modal.style.display = "none";
+    // }
 
 
     return (
@@ -102,11 +102,11 @@ function Portfolio(props) {
 
 
             </div>
-            <div id="myModal" className="modal">
-                <span className="close">&times;</span>
-                <img className="modal-content" id="img01" />
-                <div id="caption"></div>
-            </div>
+            // <div id="myModal" className="modal">
+            //     <span className="close">&times;</span>
+            //     <img className="modal-content" id="img01" />
+            //     <div id="caption"></div>
+            // </div>
         </div>
     );
 }
