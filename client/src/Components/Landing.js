3cb0392e503a6451f0b7f8f38ff2eb7ed62a6cce
Jake Peterson <a3sir86@gmail.com>
2021-04-30 10:00:29 -0600

    fixes quote box on mobile

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -7,23 +7,25 @@ function Landing(props) {
         className="section-content d-flex flex-column justify-content-between"
         id={props.id}
       >
-        <div className="container p-2 rounded quotecontainer">
-          <h2 className="p-2" id="quote">
-            I am a Developer
-          </h2>
+        <div>
+          <div className="p-2 rounded quotecontainer">
+            <h2 className="p-2" id="quote">
+              I am a Developer
+            </h2>
+          </div>
         </div>
         <div className="siteInfo">
-            <p className="pl-1 pr-2 mb-0">
-              This site is built with the MERN stack, uses Gitlab for CI/CD, and
-              is deployed in a Microk8s cluster
-            </p>
-            <p className="pl-1 pr-2 pb-0 mb-0">
-              Background image photo by Eberhard Grossgasteiger from{" "}
-              <a href="https://www.pexels.com/photo/mountain-under-starry-sky-1624504/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">
-                Pexels
-              </a>
-            </p>
-          </div>
+          <p className="pl-1 pr-2 mb-0">
+            This site is built with the MERN stack, uses Gitlab for CI/CD, and
+            is deployed in a Microk8s cluster
+          </p>
+          <p className="pl-1 pr-2 pb-0 mb-0">
+            Background image photo by Eberhard Grossgasteiger from{" "}
+            <a href="https://www.pexels.com/photo/mountain-under-starry-sky-1624504/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">
+              Pexels
+            </a>
+          </p>
+        </div>
       </div>
       <div class="snow"></div>
       <div class="snow"></div>
