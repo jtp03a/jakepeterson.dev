1329ffbe7000a69184940765c3d53aa2c36c1a46
Jake Peterson <a3sir86@gmail.com>
2021-05-04 18:58:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -114,8 +114,8 @@ mongoose
     })
     .then(() => console.log('mongoDB connected...'));
 
-    app.get('*', function (req, res, next) {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get('/*', function (req, res, next) {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 
 module.exports = app;
